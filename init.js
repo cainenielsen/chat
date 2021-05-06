f8395936e65d8f9e894b6385c1fe4c297a5d7aa7
Caine J Nielsen <caine.nielsen@caineandrebekah.com>
2021-05-06 16:44:31 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,4 +1,10 @@
 window.onload = (event) => {
   console.log('wow');
   alert('wow');
+  
+  var node = document.createElement("span");                 // Create a <li> node
+  var textnode = document.createTextNode("Water");         // Create a text node
+  node.appendChild(textnode);                              // Append the text to <li>
+  document.body.appendChild(node);     // Append <li> to <ul> with id="myList"
 };
+
