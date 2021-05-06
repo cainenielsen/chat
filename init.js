82425f362bf35cf071f744b4c5846ff361c68d71
Caine J Nielsen <caine.nielsen@caineandrebekah.com>
2021-05-06 16:55:42 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-const styles = 'height: 48px;border-radius:24px;color:white;background-color:green;font-size:16px;position:fixed;right:48px;bottom:24px;box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);';
+const styles = 'line-height:48px;padding-left:16px;padding-right:16px;height: 48px;border-radius:24px;color:white;background-color:green;font-size:16px;position:fixed;right:48px;bottom:24px;box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);';
 
 window.onload = (event) => {
   console.log('Chat loaded!');
