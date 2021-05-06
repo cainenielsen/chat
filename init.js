d6ac7ae042207885e5ab279fa1586b83f85a1201
Caine J Nielsen <caine.nielsen@caineandrebekah.com>
2021-05-06 16:54:29 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,8 +1,7 @@
 const styles = 'height: 48px;border-radius:24px;color:white;background-color:green;font-size:16px;position:fixed;right:48px;bottom:24px;box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);';
 
 window.onload = (event) => {
-  console.log('wow');
-  alert('wow');
+  console.log('Chat loaded!');
   
   var node = document.createElement("div");
   node.style.cssText += styles;
